93d790650337b3293c8b954187e2ce49b39ea165
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-04-11 01:40:11 +0300

    Upgraded for functional component

--- components/Chat.js
+++ components/Chat.js
@@ -1,38 +1,54 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View } from "react-native";
 import { GiftedChat } from "react-native-gifted-chat";
 
-export default class Chat extends React.Component {
+const Chat = ({ route, navigation }) => {
+  const [messages, setMessages] = useState([]);
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            messages: [],
-        }
-    }
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello, Developer!",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
 
     // Set the navigation header title to the name passed through navigation props
-    componentDidMount() {
-        const name = this.props.route.params.name;
-        this.props.navigation.setOptions({title: name});
-    }
+    const { name } = route.params;
+    navigation.setOptions({ title: name });
+  }, [route, navigation]);
 
-    render() {
-        // Destructure the name and color passed through navigation props
-        const { name, color } = this.props.route.params;
-        return (
-            <View style={[styles.container, {backgroundColor: color}]}>
-                <Text>Hello Chat!</Text>
-            </View>
-        );
-    }
-}
+  const onSend = (message) => {
+    setMessages((previousState) => GiftedChat.append(previousState, message));
+  };
+
+  // Destructure the name and color passed through navigation props
+  const { name, color } = route.params;
+
+  return (
+    <View style={styles.container}>
+      <GiftedChat
+        messages={messages}
+        onSend={(message) => onSend(message)}
+        user={{
+          _id: 1,
+        }}
+      />
+    </View>
+  );
+};
 
 // Styles for the component
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    }
-})
+  container: {
+    flex: 1,
+  },
+});
+
+export default Chat;
