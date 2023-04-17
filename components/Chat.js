9a81b903ead7b262784f894cc9ddae12b5d5d717
ecspecial <106328797+ecspecial@users.noreply.github.com>
2023-04-18 02:22:59 +0300

    Added caching for offline usage

--- components/Chat.js
+++ components/Chat.js
@@ -1,129 +1,159 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, View, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard } from "react-native";
-import { Bubble, GiftedChat, SystemMessage, Time, Day } from "react-native-gifted-chat";
+import { Bubble, GiftedChat, SystemMessage, Time, Day, InputToolbar } from "react-native-gifted-chat";
 import { query, onSnapshot, collection, where, orderBy, addDoc } from "firebase/firestore";
+import { async } from "@firebase/util";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Chat = ({ route, navigation, db }) => {
+const Chat = ({ route, navigation, db, isConnected }) => {
+    
     const { userID } = route.params;
     const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-
-    // Set the navigation header title to the name passed through navigation props
-    const { name } = route.params;
-    navigation.setOptions({ title: name });
-
-    const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-    const unsubMessages = onSnapshot(q, (docs) => {
-        let newMessages = [];
-        docs.forEach(doc => {
-            newMessages.push({ 
-                id: doc.id,
-                ...doc.data(),
-                createdAt: new Date(doc.data().createdAt.toMillis()),
-                });
-        });
-        setMessages(newMessages);
-    });
-
-    return () => {
-        if (unsubMessages) {unsubMessages()};
+    const cacheMessages = async (messagesToCache) => {
+        try {
+            await AsyncStorage.setItem("cached_messages", JSON.stringify(messagesToCache));
+        } catch (error) {
+            console.log(error.message);
+        }
     }
-  }, []);
 
-  const onSend = (newMessages) => {
-    addDoc(collection(db, "messages"), newMessages[0]);
-  };
+    const loadCachedMessages = async () => {
+        const cachedMessages = await AsyncStorage.getItem("cached_messages") || [];
+        setMessages(JSON.parse(cachedMessages));
+    }
 
-  const renderBubble = (props) => {
-    return (
-      <Bubble
-        {...props}
-        textStyle={{
-          right: {
-            color: "#FFF",
-          },
-          left: {
-            color: "#FFF",
-          },
-        }}
-        wrapperStyle={{
-          right: {
-            backgroundColor: "#3F6188",
-          },
-          left: {
-            backgroundColor: "#222E3A",
-          },
-        }}
-      />
-    );
-  };
+    let unsubMessages;
 
-  const renderSystemMessage = (props) => {
-    return (
-      <SystemMessage
-        {...props}
-        textStyle={{
-          color: "#FFF",
-        }}
-      />
-    );
-  };
-  
-  const renderTime = (props) => (
-    <Time
-      {...props}
-      timeTextStyle={{
-        left: {
-          color: "#FFF",
-        },
-        right: {
-          color: "#FFF",
-        },
-      }}
-    />
-  );
+    useEffect(() => {
 
-  const renderDay = (props) => {
-    return (
-      <Day
-        {...props}
-        textStyle={{
-          color: "#FFF",
-        }}
-      />
-    );
-  };
+        // Set the navigation header title to the name passed through navigation props
+        const { name } = route.params;
+        navigation.setOptions({ title: name });
 
-  // Destructure the name and color passed through navigation props
-  const { name, color } = route.params;
+        if (isConnected === true) {
 
-  return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-    <View style={[styles.container, { backgroundColor: color }]}>
-        <GiftedChat
-            messages={messages}
-            renderBubble={renderBubble}
-            renderSystemMessage={renderSystemMessage}
-            renderTime={renderTime}
-            renderDay={renderDay}
-            onSend={(message) => onSend(message)}
-            user={{
-                _id: userID,
-                name
+            if (unsubMessages) unsubMessages();
+            unsubMessages === null;
+
+            const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+            unsubMessages = onSnapshot(q, (docs) => {
+                let newMessages = [];
+                docs.forEach(doc => {
+                    newMessages.push({ 
+                        id: doc.id,
+                        ...doc.data(),
+                        createdAt: new Date(doc.data().createdAt.toMillis()),
+                    });
+                });
+                cacheMessages(newMessages);
+                setMessages(newMessages);
+        });
+        } else loadCachedMessages();
+
+        return () => {
+            if (unsubMessages) {unsubMessages()};
+        }
+    }, [isConnected]);
+
+    const onSend = (newMessages) => {
+        addDoc(collection(db, "messages"), newMessages[0]);
+    };
+
+    const renderBubble = (props) => {
+        return (
+            <Bubble
+                {...props}
+                textStyle={{
+                    right: {
+                        color: "#FFF",
+                    },
+                    left: {
+                        color: "#FFF",
+                    },
+                }}
+                wrapperStyle={{
+                    right: {
+                        backgroundColor: "#3F6188",
+                    },
+                    left: {
+                        backgroundColor: "#222E3A",
+                    },
+                }}
+            />
+        );
+    };
+
+    const renderSystemMessage = (props) => {
+        return (
+            <SystemMessage
+                {...props}
+                textStyle={{
+                    color: "#FFF",
+                }}
+            />
+        );
+    };
+
+    const renderTime = (props) => (
+        <Time
+            {...props}
+            timeTextStyle={{
+                left: {
+                    color: "#FFF",
+                },
+                right: {
+                    color: "#FFF",
+                },
             }}
         />
-      { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
-    </View>
-    </TouchableWithoutFeedback>
-  );
+    );
+
+    const renderDay = (props) => {
+        return (
+            <Day
+                {...props}
+                textStyle={{
+                    color: "#FFF",
+                }}
+            />
+        );
+     };
+
+    const renderInputToolbar = (props) => {
+        if (isConnected === true) return <InputToolbar {...props} />;
+        else return null
+    }
+
+    // Destructure the name and color passed through navigation props
+    const { name, color } = route.params;
+
+    return (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={[styles.container, { backgroundColor: color }]}>
+            <GiftedChat
+                messages={messages}
+                renderBubble={renderBubble}
+                renderInputToolbar={renderInputToolbar}
+                renderTime={renderTime}
+                renderDay={renderDay}
+                onSend={(message) => onSend(message)}
+                user={{
+                    _id: userID,
+                    name
+                }}
+            />
+        { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+        </View>
+        </TouchableWithoutFeedback>
+    );
 };
 
 // Styles for the component
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+    },
 });
 
 export default Chat;
